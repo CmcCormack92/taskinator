fb3cbbcbbf469f43396f0314041ec74a8969840b
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-05 21:32:27 -0400

    prevent default added to event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);
+    console.log(event)
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 
 
 
