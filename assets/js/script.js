fe4e8311dcf6c2d4effdf21d34a43edae18b94f0
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-05 22:17:37 -0400

    input and select values function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
 
     event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl);
-    console.log(event)
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formE1.addEventListener("submit", createTaskHandler);
